a5cccc37cf5b33749fc2259681b58c45470dc2f9
nvng <nvngliu@gmail.com>
2023-06-13 05:20:08 +0000

    a

--- DBServer/src/App.cc
+++ DBServer/src/App.cc
@@ -16,18 +16,44 @@ App* GetApp()
 App::App(const std::string& appName)
 	: SuperType(appName)
 {
-	DBMgr::CreateInstance();
+        DBMgr::CreateInstance();
 }
 
 App::~App()
 {
-	DBMgr::DestroyInstance();
+        DBMgr::DestroyInstance();
 }
 
 bool App::Init()
 {
 	LOG_FATAL_IF(!SuperType::Init(E_ST_DB), "super init fail!!!");
-	LOG_FATAL_IF(!DBMgr::GetInstance()->Init(), "dbmgr proc mgr init fail!!!");
+        LOG_FATAL_IF(!DBMgr::GetInstance()->Init(), "dbmgr proc mgr init fail!!!");
+
+        GetSteadyTimer().StartWithRelativeTimeForever(1.0, [](TimedEventItem& eventData) {
+                static int64_t loadVersionCnt = 0;
+                static int64_t loadVersionSize = 0;
+                static int64_t loadCnt = 0;
+                static int64_t loadSize = 0;
+                static int64_t saveCnt = 0;
+                static int64_t saveSize = 0;
+
+                LOG_INFO_IF(true, "avg[{}] lvc[{}] lvs[{:5f}] lc[{}] ls[{:5f}] sc[{}] ss[{:5f}]",
+                            GetFrameController().GetAverageFrameCnt(),
+                            (GetApp()->_loadVersionCnt - loadVersionCnt) / 1000.0,
+                            (GetApp()->_loadVersionSize - loadVersionSize) / (1024.0 * 1024.0),
+                            (GetApp()->_loadCnt - loadCnt) / 1000.0,
+                            (GetApp()->_loadSize - loadSize) / (1024.0 * 1024.0),
+                            (GetApp()->_saveCnt - saveCnt) / 1000.0,
+                            (GetApp()->_saveSize - saveSize) / (1024.0 * 1024.0)
+                           );
+
+                loadVersionCnt = GetApp()->_loadVersionCnt;
+                loadVersionSize = GetApp()->_loadVersionSize;
+                loadCnt = GetApp()->_loadCnt;
+                loadSize = GetApp()->_loadSize;
+                saveCnt = GetApp()->_saveCnt;
+                saveSize = GetApp()->_saveSize;
+        });
 
 	_startPriorityTaskList->AddFinalTaskCallback([]() {
 		auto proc = NetProcMgr::GetInstance()->Dist(1);
@@ -35,6 +61,15 @@ bool App::Init()
 	});
 
 	_stopPriorityTaskList->AddFinalTaskCallback([]() {
+                NetProcMgr::GetInstance()->ForeachProc([](const auto& proc) {
+                        proc->PostTask([proc]() {
+                                proc->stopAllListener();
+                        });
+                        proc->_sesList.Foreach([](auto ses) {
+                                ses->AsyncClose(E_SCRT_ServiceTerminate);
+                        });
+                });
+
 		DBMgr::GetInstance()->Terminate();
 		DBMgr::GetInstance()->WaitForTerminate();
 	});
--- DBServer/src/App.h
+++ DBServer/src/App.h
@@ -9,6 +9,12 @@ class App : public AppBase, public Singleton<App>
   friend class Singleton<App>;
 
 public :
+  std::atomic_int64_t _loadVersionCnt;
+  std::atomic_int64_t _loadVersionSize;
+  std::atomic_int64_t _loadCnt;
+  std::atomic_int64_t _loadSize;
+  std::atomic_int64_t _saveCnt;
+  std::atomic_int64_t _saveSize;
   bool Init();
 };
 
