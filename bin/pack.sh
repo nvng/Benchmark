0092b7fd5e06c6fc0baf8ec8d6071dcc7c6ac8a5
nvng <nvngliu@gmail.com>
2024-03-16 03:58:03 +0000

    a

--- bin/pack.sh
+++ bin/pack.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-export cmake_compiler="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
+export cmake_compiler="-DCMAKE_CXX_COMPILER=clang++"
+# export cmake_compiler="-DCMAKE_CXX_COMPILER=g++"
 if [[ "$1" == "d" || "$1" == "debug" ]]; then
+        build_type=$1
         export build_flag=-DCMAKE_BUILD_TYPE=Debug
 else
-        ccache --clear
+        build_type=9999
         export build_flag=-DCMAKE_BUILD_TYPE=RelWithDebInfo
+        ccache --clear
 fi
 
-cpus=`grep -c ^processor /proc/cpuinfo`
-export build_jobs=$[ $cpus / 4 ]
+if [[ `uname` == 'Darwin' ]]; then
+    echo "Mac OS"
+    cpus=8
+    export build_jobs=$cpus
+elif [[ `uname` == 'Linux' ]]; then
+    echo "Linux"
+    cpus=`grep -c ^processor /proc/cpuinfo`
+    export build_jobs=$[ $cpus / 2 ]
+fi
 
 dirs=`ls -d ../*Server ../Benchmark | awk -F/ '{print $2}'`
 rm -f $dirs
@@ -31,23 +41,40 @@ build_func(){
         cmake $build_flag .. $cmake_compiler
         make -j$build_jobs
         cd ../../bin
-        cp ../$pg/${pg}.out ./
+        cp ../$pg/$pg.out ./
         rm -rf ../$pg/$dir
 
-        gzexe $pg.out
-        rm $pg.out~
+        # gzexe $pg.out
+        # rm $pg.out~
 }
 
 export -f build_func
 
-parallel build_func ::: $dirs
+if [ $NVNGLIB_INCLUDE_DIR ]; then
+        cd $NVNGLIB_INCLUDE_DIR && ./pack.sh $build_type $cmake_compiler && cd -
+fi
 
-# for pg in ${dirs[*]}
-# do
-#     build_func $pg
-# done
+if [[ `uname` == 'Darwin' ]]; then
+    echo "Mac OS"
 
-unset cmake_compiler
+    for pg in ${dirs[*]}
+    do
+            build_func $pg
+    done
+elif [[ `uname` == 'Linux' ]]; then
+    echo "Linux"
+
+    if [ "$cmake_compiler" == "-DCMAKE_CXX_COMPILER=g++" ]; then
+            export build_jobs=$[ $cpus / 2 ]
+            for pg in ${dirs[*]}
+            do
+                    build_func $pg
+            done
+    else
+            parallel build_func ::: $dirs
+    fi
+fi
+
 unset build_flag
 unset build_jobs
 unset build_func
